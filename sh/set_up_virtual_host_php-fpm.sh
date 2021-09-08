9669800c4da237e7a18871132d3c7a67f6f4b8cc
khanhpnk <phamngockhanh262@gmail.com>
2021-09-08 19:12:04 +0700

    virtual host sh

--- sh/set_up_virtual_host_php-fpm.sh
+++ sh/set_up_virtual_host_php-fpm.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# Example: ./set_up_virtual_host.sh la7.lc la7/public
+# Example: ./set_up_virtual_host_php-fpm.sh la7.lc la7/public php7.3
 
 HOST_NAME=$1
 VHOST_FILE=$HOST_NAME.conf
 DOCUMENT_ROOT=$2
+PHP_VERSION=$3
 
 # Create the Virtual Host File
 sudo bash -c "cat > /etc/apache2/sites-available/$VHOST_FILE <<EOL
@@ -12,6 +13,11 @@ sudo bash -c "cat > /etc/apache2/sites-available/$VHOST_FILE <<EOL
     ServerAdmin phamngockhanh262@gmail.com
     ServerName $HOST_NAME
     DocumentRoot /var/www/html/$DOCUMENT_ROOT
+
+    <FilesMatch \.php$>
+        #Apache 2.4.10+ use SetHandler to run PHP as fastCGI process server
+        SetHandler \"proxy:unix:/run/php/$PHP_VERSION-fpm.sock|fcgi://localhost\"
+    </FilesMatch>
 
     ErrorLog /var/log/apache2/error.log
     CustomLog /var/log/apache2/access.log combined
